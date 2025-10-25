a074934c1265725467d6931dd5d85227aaaa638c
senuracmps <senuracmps@gmail.com>
2025-10-25 21:26:54 +0530

    City Management menu setup

--- src/Main.java
+++ src/Main.java
@@ -70,4 +70,38 @@ public class Main {
 
         sc.close();
     }
+
+    //City Management
+    static void cityManagement() {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("----------CITY MANAGEMENT----------");
+            System.out.println("1.Add New City");
+            System.out.println("2.Rename City");
+            System.out.println("3.Remove City");
+            System.out.println("4.Back");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+            sc.nextLine();
+
+            switch(choice) {
+                case 1:
+                    addNewCity(sc);
+                    break;
+                case 2:
+                    renameCity(sc);
+                    break;
+                case 3:
+                    removeCity(sc);
+                    break;
+                case 4:
+                    System.out.println("Returning");
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+        } while(choice != 4);
+    }
 }
